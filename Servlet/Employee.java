091321d85bdb82da2763b6bf1c8d50a29a72509d
srinidhi0506 <bhonagirisrinidhi@gmail.com>
2026-04-18 12:21:56 +0530

    Add Employee class with properties and methods

---
+++ Servlet/Employee.java
@@ -0,0 +1,27 @@
+package coma4;
+
+public class Employee {
+        private String empname;
+        private int eid;
+        private int esal;
+		public String getEmpname() {
+			return empname;
+		}
+		public void setEmpname(String empname) {
+			this.empname = empname;
+		}
+		public int getEid() {
+			return eid;
+		}
+		public void setEid(int eid) {
+			this.eid = eid;
+		}
+		public int getEsal() {
+			return esal;
+		}
+		public void setEsal(int esal) {
+			this.esal = esal;
+		}
+        
+        
+}
